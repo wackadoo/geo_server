84b561b20efe3aa2ce8f4c39e3d472503d1c0e38
Patrick <p@trick-fox.de>
2013-08-15 22:34:00 +0200

    create character if not exists and token is valid

--- app/models/fundamental/character.rb
+++ app/models/fundamental/character.rb
@@ -1,2 +1,5 @@
 class Fundamental::Character < ActiveRecord::Base
+
+  has_one  :position, :class_name => 'Fundamental::CharacterPosition',  :foreign_key => 'character_id',  :inverse_of => :character
+
 end
