d46a9c2d6acc35e8aad98e17d4075e02541ed26c
Patrick <p@trick-fox.de>
2013-08-15 21:03:28 +0200

    added action for logging character geo position

---
+++ app/controllers/action/fundamental/log_geo_position_actions_controller.rb
@@ -0,0 +1,25 @@
+class Action::Fundamental::LogGeoPositionActionsController < ApplicationController
+  layout 'action'
+
+  #before_filter :authenticate
+
+  def create
+    #raise BadRequestError.new('no current character') if current_character.nil?
+    if params[:log_geo_position_action].nil? ||
+        params[:log_geo_position_action][:longitude].blank? ||
+        params[:log_geo_position_action][:latitude].blank? ||
+        params[:log_geo_position_action][:character_id].blank?
+      raise BadRequestError.new('missing params')
+    end
+
+    geo_position = Fundamental::CharacterPosition.find_or_initialize_by_character_id(params[:log_geo_position_action][:character_id])
+    geo_position.longitude = params[:log_geo_position_action][:longitude]
+    geo_position.latitude =  params[:log_geo_position_action][:latitude]
+    geo_position.save
+
+    respond_to do |format|
+      format.json { render json: {}, status: :ok }
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 GeoServer::Application.routes.draw do
-
-
   scope "/geo_server" do
     scope "(:locale)", :locale => /en|de/ do   
       
@@ -14,7 +12,12 @@ GeoServer::Application.routes.draw do
         resources :daily_position_stats
         resources :character_positions
       end
-      
+
+      namespace :action do
+        namespace :fundamental do
+          resources :log_geo_position_actions, :only => [ :create ]
+        end
+      end
 
       resource :action, :only => [ :show ]
       
