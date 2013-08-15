70c19a9ab77249cb0e8740cf2b1a0d55cefd03c1
salange <sascha@5dlab.com>
2013-08-15 23:59:37 +0200

    treasures

---
+++ app/controllers/action/treasure/open_treasure_actions_controller.rb
@@ -0,0 +1,32 @@
+class Action::Treasure::OpenTreasureActionsController < ApplicationController
+  layout 'action'
+
+  before_filter :authenticate
+
+  def create
+    raise BadRequestError.new('no treasure id sent') if params[:id].blank?
+    raise BadRequestError.new('no character id sent') if params[:identifier].blank?
+    
+    @character = Fundamental::Character.find_by_identifier(params[:identifier])
+    
+    raise BadRequestError.new('no character') unless @character
+    raise BadRequestError.new('no position for character') if @character.latitude.nil? || @character.longitude.nil?
+    
+    @treasure = Treasure::Treasure.find_by_id(params[:id])
+    raise NotFoundError.new('Treasure Not Found') if @treasure.nil?
+
+    radius_earth = 6371                      # in km
+    
+    distance = Mapping::Util.map_distance(@character.latitude, @character.longitude, @treasure.latitude, @treasure.longitude)
+                             
+    logger.debug "trying to grab treasure #{ @treasure.id } in #{ distance }km distance of character #{ @character.name }" 
+                         
+    if distance < 0.2  # 200 m distance is ok
+      @treasure.destroy
+      head :ok
+    else 
+      head :forbidden
+    end
+  end
+  
+end
---
+++ lib/mapping/util.rb
@@ -0,0 +1,22 @@
+module Mapping
+  
+  module Util
+  
+    # calculates the distance between two map coordinates.
+    def self.map_distance(lat1, lon1, lat2, lon2)
+    
+      radius_earth = 6371.0              # in km
+    
+      lat1r = lat1 / 180.0 * Math::PI    # convert degrees to radians
+      lat2r = lat2 / 180.0 * Math::PI
+
+      lon1r = lon1 / 180.0 * Math::PI
+      lon2r = lon2 / 180.0 * Math::PI
+        
+      return Math.acos(Math.sin(lat1r)*Math.sin(lat2r) +       # using the most simple formula. the result's precision highly depends on float's precision
+                       Math.cos(lat1r)*Math.cos(lat2r) *
+                       Math.cos(lon2r-lon1r)) * radius_earth ;
+    
+    end
+  end
+end
