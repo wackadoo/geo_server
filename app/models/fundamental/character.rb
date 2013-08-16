3b0e67ec1ba85cb31eddb557c1bd20f8b3ee40e5
Patrick <p@trick-fox.de>
2013-08-16 23:31:45 +0200

    added scopes for geo character

--- app/models/fundamental/character.rb
+++ app/models/fundamental/character.rb
@@ -4,6 +4,9 @@ class Fundamental::Character < ActiveRecord::Base
 
   IDENTIFIER_REGEX = /[a-z]{16}/i
 
+  scope :with_lat_and_long, where('latitude is not null and longitude is not null')
+  scope :recently_updated,  where('location_updated_at is not null and location_updated_at > ?', Time.now - GEO_SERVER_CONFIG['recently_updated_interval'].minutes)
+
   def self.find_by_id_or_identifier(user_identifier)
     identity = Fundamental::Character.find_by_id(user_identifier) if Fundamental::Character.valid_id?(user_identifier)
     identity = Fundamental::Character.find_by_identifier(user_identifier) if identity.nil? && Fundamental::Character.valid_identifier?(user_identifier)
