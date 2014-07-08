90e4cae03a37178b9f84ea9f0b72f30461cd29cc
Patrick Fox <patrick.fox@meinestadt.de>
2014-07-08 19:10:37 +0200

    prepared for round 6

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :web, "gs05.wack-a-doo.de"                          # Your HTTP server, Apache/etc
-role :app, "gs05.wack-a-doo.de"                          # This may be the same as your `Web` server
-role :db,  "gs05.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
+role :web, "gs06.wack-a-doo.de"                          # Your HTTP server, Apache/etc
+role :app, "gs06.wack-a-doo.de"                          # This may be the same as your `Web` server
+role :db,  "gs06.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
 
 set :rails_env, 'production'
 set :port, 5775                                          # until 5775 is working!
