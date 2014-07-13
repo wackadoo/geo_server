508fa0ab751e30c0130c5af5a21ad1da5a14a7fd
Patrick Fox <patrick.fox@meinestadt.de>
2014-07-13 18:32:21 +0200

    moved round 5 to branch round5

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,5 @@ role :db,  "gs05.wack-a-doo.de", :primary => true        # This is where Rails m
 set :rails_env, 'production'
 set :port, 5775                                          # until 5775 is working!
 
-set :branch,    "master"
+set :branch,    "round5"
 
